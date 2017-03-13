c18d60603060074640553d65263f31bde786b40d
Zac <zac428@gmail.com>
2017-03-12 21:48:38 -0500

    changed secret key and ignored it

--- django/airontime/settings.py
+++ django/airontime/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/1.10/ref/settings/
 """
 
 import os
+import secret_key
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'p*p41j3oq&s2j9%pj2r1adtt9fv65*u513=y!^7&3*$l@gax%z'
+SECRET_KEY = secret_key.key
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
