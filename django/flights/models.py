92d8df2826b577a052285a3bc9db18ff17a5a67c
[Amber Sahdev] <asahdev2@illinois.edu>
2017-06-13 18:22:13 +0530

    populated db, applied migrations

--- django/flights/models.py
+++ django/flights/models.py
@@ -1,5 +1,24 @@
 from django.db import models
 
+class Flight(models.Model):
+    year = models.PositiveSmallIntegerField()
+    month = models.PositiveSmallIntegerField()
+    fl_date = models.DateTimeField()
+    carrier = models.CharField(max_length = 2)
+    origin_airport_id = models.PositiveIntegerField()
+    dest_airport_id = models.PositiveIntegerField()
+    dep_delay_new = models.PositiveSmallIntegerField()
+    arr_delay_new = models.PositiveSmallIntegerField()
+    cancelled = models.PositiveSmallIntegerField()
+    diverted = models.PositiveSmallIntegerField()
+    unique_carrier = models.CharField(max_length = 2)
+    fl_num = models.PositiveSmallIntegerField()
+    origin = models.CharField(max_length = 3)
+    dest = models.CharField(max_length = 3)
+    OTR = models.PositiveSmallIntegerField()
+
+
+'''
 #HiddenFlights: Class for items that are unique to details that we don't plan to show on the search page
 class HiddenFlight(models.Model):
     year = models.PositiveSmallIntegerField()
@@ -19,3 +38,5 @@ class DisplayFlight(models.Model):
     fl_num = models.PositiveSmallIntegerField()
     origin = models.CharField(max_length = 3)
     dest = models.CharField(max_length = 3)
+    OTR = models.PositiveSmallIntegerField()
+'''
--- django/import_data.py
+++ django/import_data.py
@@ -11,7 +11,7 @@ sys.path.append('/django')
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "airontime.settings")
 django.setup()
 
-from flights.models import HiddenFlight, DisplayFlight
+from flights.models import Flight
 import csv
 
 
@@ -21,27 +21,12 @@ with open('sanitized_flight_data.csv') as f:
     for row in reader:
         count+=1
         print(count)
-        
-        HiddenFlight.year = row[0],
-        HiddenFlight.month = row[1],
-        HiddenFlight.fl_date = row[2],
-        DisplayFlight.unique_carrier = row[3],
-        DisplayFlight.fl_num = row[5],
-        HiddenFlight.origin_airport_id = row[6],
-        DisplayFlight.origin = row[7],
-        HiddenFlight.dest_airport_id = row[8],
-        DisplayFlight.dest = row[9],
-        HiddenFlight.dep_delay_new = row[10],
-        HiddenFlight.arr_delay_new = row[11],
-        HiddenFlight.cancelled = row[12],
-        HiddenFlight.diverted = row[13]
-
-        '''
         _, created = Flight.objects.get_or_create(
             year = row[0],
             month = row[1],
             fl_date = row[2],
             unique_carrier = row[3],
+            carrier = row[4],
             fl_num = row[5],
             origin_airport_id = row[6],
             origin = row[7],
@@ -50,6 +35,46 @@ with open('sanitized_flight_data.csv') as f:
             dep_delay_new = row[10],
             arr_delay_new = row[11],
             cancelled = row[12],
-            diverted = row[13]
+            diverted = row[13],
+            OTR = 0
         )
-        '''
+
+
+
+
+'''
+dataReader = csv.reader(open('sanitized_flight_data.csv'), delimiter=',')
+count = 0
+
+for row in dataReader:
+    count+=1
+    print(count)
+    DisplayFlight = DisplayFlight()
+    DisplayFlight.unique_carrier = row[3]
+    DisplayFlight.fl_num = row[5]
+    DisplayFlight.origin = row[7]
+    DisplayFlight.dest = row[9]
+    DisplayFlight.OTR = 0
+    DisplayFlight.save()
+
+
+dataReader = csv.reader(open('sanitized_flight_data.csv'), delimiter=',')
+count = 0
+
+for row in dataReader:
+    count+=1
+    print(count)
+    HiddenFlight = HiddenFlight()
+    HiddenFlight.year = row[0]
+    HiddenFlight.month = row[1]
+    HiddenFlight.fl_date = row[2]
+    HiddenFlight.carrier = row[4]
+    HiddenFlight.origin_airport_id = row[6]
+    HiddenFlight.dest_airport_id = row[8]
+    HiddenFlight.dep_delay_new = row[10]
+    HiddenFlight.arr_delay_new = row[11]
+    HiddenFlight.cancelled = row[12]
+    HiddenFlight.diverted = row[13]
+    HiddenFlight.save()
+
+'''
